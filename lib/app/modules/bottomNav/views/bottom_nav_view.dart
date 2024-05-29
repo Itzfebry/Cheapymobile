c534049d8698cfb2c2dfec11dee9a02f94b3d78a
RiaKurnia8 <e31222738@student.polije.ac.id>
2024-05-29 12:16:17 +0700

    Frontend nav profile

--- lib/app/modules/bottomNav/views/bottom_nav_view.dart
+++ lib/app/modules/bottomNav/views/bottom_nav_view.dart
@@ -1,56 +1,56 @@
-// import 'package:cheapy/app/modules/cart/views/cart_view.dart';
-// import 'package:cheapy/app/modules/home/views/home_view.dart';
-// import 'package:cheapy/app/modules/product/views/product_view.dart';
-// import 'package:cheapy/app/modules/profile/views/profile_view.dart';
-// import 'package:cheapy/app/modules/riwayat/views/riwayat_view.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import '../controllers/bottom_nav_controller.dart';
+import 'package:cheapy/app/modules/cart/views/cart_view.dart';
+import 'package:cheapy/app/modules/home/views/home_view.dart';
+import 'package:cheapy/app/modules/product/views/product_view.dart';
+import 'package:cheapy/app/modules/profile/views/profile_view.dart';
+import 'package:cheapy/app/modules/riwayat/views/riwayat_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/bottom_nav_controller.dart';
 
-// class BottomNavView extends StatelessWidget {
-//   final BottomNavController controller = Get.put(BottomNavController());
+class BottomNavView extends StatelessWidget {
+  final BottomNavController controller = Get.put(BottomNavController());
 
-//   final List<Widget> screens = [
-//     HomeView(), // Ganti dengan halaman Home yang Anda miliki
-//     ProductView(), // Ganti dengan halaman Product yang Anda miliki
-//     CartView(), // Ganti dengan halaman Cart yang Anda miliki
-//     RiwayatView(), // Ganti dengan halaman Riwayat yang Anda miliki
-//     ProfileView(), // Ganti dengan halaman Profile yang Anda miliki
-//   ];
+  final List<Widget> screens = [
+    HomeView(), // Ganti dengan halaman Home yang Anda miliki
+    ProductView(), // Ganti dengan halaman Product yang Anda miliki
+    CartView(), // Ganti dengan halaman Cart yang Anda miliki
+    RiwayatView(), // Ganti dengan halaman Riwayat yang Anda miliki
+    ProfileView(), // Ganti dengan halaman Profile yang Anda miliki
+  ];
 
-//   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       body: Obx(() => screens[controller.selectedIndex.value]),
-// //       bottomNavigationBar: Obx(() => BottomNavigationBar(
-// //             currentIndex: controller.selectedIndex.value,
-// //             onTap: controller.changeTabIndex,
-// //             selectedItemColor: Colors.black, // Warna ikon yang dipilih (hitam)
-// //             unselectedItemColor:
-// //                 Colors.grey, // Warna ikon yang tidak dipilih (hitam)
-// //             items: const [
-// //               BottomNavigationBarItem(
-// //                 icon: Icon(Icons.home),
-// //                 label: 'Home',
-// //               ),
-// //               BottomNavigationBarItem(
-// //                 icon: Icon(Icons.shopping_bag),
-// //                 label: 'Product',
-// //               ),
-// //               BottomNavigationBarItem(
-// //                 icon: Icon(Icons.shopping_cart),
-// //                 label: 'Cart',
-// //               ),
-// //               BottomNavigationBarItem(
-// //                 icon: Icon(Icons.list_alt),
-// //                 label: 'Riwayat',
-// //               ),
-// //               BottomNavigationBarItem(
-// //                 icon: Icon(Icons.person),
-// //                 label: 'Profile',
-// //               ),
-// //             ],
-// //           )),
-// //     );
-// //   }
-// // }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(() => screens[controller.selectedIndex.value]),
+      bottomNavigationBar: Obx(() => BottomNavigationBar(
+            currentIndex: controller.selectedIndex.value,
+            onTap: controller.changeTabIndex,
+            selectedItemColor: Colors.black, // Warna ikon yang dipilih (hitam)
+            unselectedItemColor:
+                Colors.grey, // Warna ikon yang tidak dipilih (hitam)
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag),
+                label: 'Product',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                label: 'Cart',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt),
+                label: 'Riwayat',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+          )),
+    );
+  }
+}
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,138 +1,138 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
-import '../controllers/login_controller.dart';
+// import '../controllers/login_controller.dart';
 
-class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    LoginController controller = Get.put(LoginController());
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment
-              .start, // Align children to the start (left side)
-          crossAxisAlignment: CrossAxisAlignment
-              .start, // Align children to the start (left side)
-          children: [
-            Text(
-              "Sign In",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              "Silahkan melakukan Sign In",
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: controller.emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
-                hintText: "Masukkan Email",
-                hintStyle: TextStyle(
-                    color: Colors.grey), // Hint text color set to gray
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color: Colors.grey)), // Border color set to gray
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color:
-                            Colors.grey)), // Focused border color set to gray
-                suffixIcon: Icon(Icons.email,
-                    color: Colors.grey), // Icon color set to gray
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: controller.passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
-                hintText: "Masukkan Password",
-                hintStyle: TextStyle(
-                    color: Colors.grey), // Hint text color set to gray
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color: Colors.grey)), // Border color set to gray
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color:
-                            Colors.grey)), // Focused border color set to gray
-                suffixIcon: Icon(Icons.lock,
-                    color: Colors.grey), // Icon color set to gray
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.end, // Align the InkWell to the right
-              children: [
-                InkWell(
-                  onTap: () {
-                    controller.login();
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.black, width: 2.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.arrow_forward,
-                        size: 24.0,
-                        color:
-                            Colors.white, // Set the color of the icon to white
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Belum Mendaftar?",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: InkWell(
-                    onTap: () {
-                      Get.offAllNamed("/register");
-                    },
-                    child: Text(
-                      "Register",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class LoginView extends GetView<LoginController> {
+//   const LoginView({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     LoginController controller = Get.put(LoginController());
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(''),
+//         centerTitle: true,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(10.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment
+//               .start, // Align children to the start (left side)
+//           crossAxisAlignment: CrossAxisAlignment
+//               .start, // Align children to the start (left side)
+//           children: [
+//             Text(
+//               "Sign In",
+//               style: TextStyle(
+//                 fontSize: 20,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             Text(
+//               "Silahkan melakukan Sign In",
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             TextFormField(
+//               controller: controller.emailController,
+//               decoration: InputDecoration(
+//                 labelText: "Email",
+//                 hintText: "Masukkan Email",
+//                 hintStyle: TextStyle(
+//                     color: Colors.grey), // Hint text color set to gray
+//                 border: OutlineInputBorder(
+//                     borderSide: BorderSide(
+//                         color: Colors.grey)), // Border color set to gray
+//                 focusedBorder: OutlineInputBorder(
+//                     borderSide: BorderSide(
+//                         color:
+//                             Colors.grey)), // Focused border color set to gray
+//                 suffixIcon: Icon(Icons.email,
+//                     color: Colors.grey), // Icon color set to gray
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             TextFormField(
+//               controller: controller.passwordController,
+//               decoration: InputDecoration(
+//                 labelText: "Password",
+//                 hintText: "Masukkan Password",
+//                 hintStyle: TextStyle(
+//                     color: Colors.grey), // Hint text color set to gray
+//                 border: OutlineInputBorder(
+//                     borderSide: BorderSide(
+//                         color: Colors.grey)), // Border color set to gray
+//                 focusedBorder: OutlineInputBorder(
+//                     borderSide: BorderSide(
+//                         color:
+//                             Colors.grey)), // Focused border color set to gray
+//                 suffixIcon: Icon(Icons.lock,
+//                     color: Colors.grey), // Icon color set to gray
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             Row(
+//               mainAxisAlignment:
+//                   MainAxisAlignment.end, // Align the InkWell to the right
+//               children: [
+//                 InkWell(
+//                   onTap: () {
+//                     controller.login();
+//                   },
+//                   child: Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.black,
+//                       shape: BoxShape.circle,
+//                       border: Border.all(color: Colors.black, width: 2.0),
+//                     ),
+//                     child: Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: Icon(
+//                         Icons.arrow_forward,
+//                         size: 24.0,
+//                         color:
+//                             Colors.white, // Set the color of the icon to white
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             SizedBox(
+//               height: 30,
+//             ),
+//             Row(
+//               children: [
+//                 Text(
+//                   "Belum Mendaftar?",
+//                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: InkWell(
+//                     onTap: () {
+//                       Get.offAllNamed("/register");
+//                     },
+//                     child: Text(
+//                       "Register",
+//                       style: TextStyle(
+//                         fontSize: 18,
+//                         color: Colors.red,
+//                       ),
+//                     ),
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
